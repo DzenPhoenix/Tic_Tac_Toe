6eaf8e7db96ea21fb692519a2b5d2a16ac857d3e
Denys Maksymenko <denys.maksymenko@freshcodeit.com>
2022-05-10 19:58:32 +0300

    core-3 Add field model, handleClick function

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/models/cell_model.dart';
+import 'package:tic_tac_toe/models/field_model.dart';
 import './Field/field.dart';
 
 class TicTacToeApp extends StatefulWidget {
   final int fieldSize;
-  const TicTacToeApp({Key? key, required this.fieldSize}) : super(key: key);
+  final FieldModel fieldState = FieldModel.generateDefault();
+  TicTacToeApp({Key? key, required this.fieldSize}) : super(key: key) {
+    fieldState.generateNewField(fieldSize: fieldSize);
+  }
 
   @override
   _TicTacToeState createState() => _TicTacToeState();
 }
 
 class _TicTacToeState extends State<TicTacToeApp> {
-  // add handle click
   @override
   Widget build(BuildContext context) {
-    List<List<CellModel>> fieldState = List<List<CellModel>>.generate(
-        widget.fieldSize,
-        (xIdx) => List<CellModel>.generate(widget.fieldSize,
-            (yIdx) => CellModel(posX: xIdx, posY: yIdx, isZero: false)));
+    void cellHandleClick({required CellModel targetCell}) {
+      bool currentState = targetCell.getIsZero();
+      setState(() {
+        widget.fieldState
+            .getCell(posX: targetCell.getX(), posY: targetCell.getY())
+            .setIsZero(!currentState);
+      });
+    }
+
     return MaterialApp(
-        home: Scaffold(body: Field(fieldSize: widget.fieldSize)));
+        home: Scaffold(
+            body: Field(
+      fieldSize: widget.fieldSize,
+      handleClick: cellHandleClick,
+      fieldState: widget.fieldState,
+    )));
   }
 }
